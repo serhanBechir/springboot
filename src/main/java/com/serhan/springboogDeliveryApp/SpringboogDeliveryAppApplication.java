b305ebf5b12fd33c4e540df949c8a6e53133bcc3
serhanBechir <bechir.serhan@gmail.com>
2021-08-19 14:45:21 +0300

    first

---
+++ src/main/java/com/serhan/springboogDeliveryApp/SpringboogDeliveryAppApplication.java
@@ -0,0 +1,13 @@
+package com.serhan.springboogDeliveryApp;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringboogDeliveryAppApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringboogDeliveryAppApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/serhan/springboogDeliveryApp/SpringboogDeliveryAppApplicationTests.java
@@ -0,0 +1,13 @@
+package com.serhan.springboogDeliveryApp;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringboogDeliveryAppApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
